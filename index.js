e0514d18645549e5c5e46032a53223e370777e52
José Lucas <joselucas.freitas@webjump.com.br>
2024-10-11 15:34:09 -0300

    feat: game is ok

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
+const colors = ['yellow', 'red', 'pink', 'green']
+
 window.addEventListener('load', () => {
+
+  let winnerDeclared = false
 
   for (let i = 0; i < 5; i++) {
     const squareYellow = document.createElement('div')
@@ -16,5 +20,93 @@ window.addEventListener('load', () => {
   const red = document.querySelector('.red').getElementsByTagName('div')
   const pink = document.querySelector('.pink').getElementsByTagName('div')
   const green = document.querySelector('.green').getElementsByTagName('div')
+
+  const rollDice = document.getElementById('roll-dice')
+  const collorRoll = document.getElementById('color-roll')
+
+  const generateRandomColor = () => {
+    const index = Math.floor(Math.random() * colors.length)
+    return colors[index]
+  }
+
+  const generateRandomImage = () => {
+    const index = Math.floor(Math.random() * 4) + 1
+    return `./public/happy${index}.gif`
+  }
   
+  const updateDivColor = (mainDiv, color, portuguese) => {
+    let incorrectColor = []
+    for (let i = 1; i < mainDiv.length; i++) {
+      if(mainDiv[i].style.backgroundColor !== color){
+        incorrectColor.push(mainDiv[i])
+      }
+    }
+
+    if(incorrectColor.length > 0){
+      incorrectColor[0].style.backgroundColor = color
+    }
+
+    if(incorrectColor.length === 1){
+      rollDice.disabled = true;
+      winnerDeclared = true
+      const modal = document.getElementById('modal')
+      modal.style.display = 'flex'
+      document.getElementById('modal-content').style.backgroundColor = color
+      document.getElementById('canvas').style.opacity = 0.4
+      document.getElementById('controls').style.opacity = 0.4
+      const winnerName = document.getElementById('winner-name')
+      winnerName.textContent = portuguese
+
+      document.getElementById('winner-image').src = generateRandomImage()
+    }
+
+  }
+  
+  const translateColor = (color) => {
+    let portuguese;
+    switch (color) {
+      case 'yellow':
+        portuguese = 'Amarelo';
+        updateDivColor(yellow, 'yellow', portuguese)
+        return portuguese
+      case 'red':
+        portuguese = 'Vermelho';
+        updateDivColor(red, 'red', portuguese)
+        return portuguese
+      case 'pink':
+        portuguese = 'Rosa';
+        updateDivColor(pink, 'pink', portuguese)
+        return portuguese
+      case 'green':
+        portuguese = 'Verde';
+        updateDivColor(green, 'green', portuguese)
+        return portuguese
+    }
+  }
+
+  rollDice.addEventListener('click', () => {
+    if (winnerDeclared) return;
+    let i = 0;
+
+    const dice = setInterval(() => {
+      rollDice.disabled = true;
+
+        collorRoll.textContent = "Escolhendo cor...";
+        chooseColor = generateRandomColor();
+        rollDice.style.backgroundColor = chooseColor;
+
+        i++;
+
+        if (i >= colors.length * 2) {
+            clearInterval(dice);
+            collorRoll.textContent = translateColor(chooseColor);
+            rollDice.disabled = false;
+        }
+    }, 100);
+  });
+  
+})
+
+document.getElementById('play-again').addEventListener('click', () => {
+  window.location.reload();
 })
